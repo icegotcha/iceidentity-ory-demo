d3437553f5d5909c3bfaf854e46691ccb017170f
Kotchakorn Wongwisutigul <icekotcha@gmail.com>
2023-07-30 09:22:40 +0700

    build: :wrench: Add primary colors to tailwindconfig

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,7 +3,13 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f50057',
+        'primary-dark': '#bd0041',
+        'primary-light': '#ffd9dc',
+      },
+    },
   },
   plugins: [],
 }
