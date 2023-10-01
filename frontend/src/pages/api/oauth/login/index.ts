1146bc2f433b003a257155daf8c866feebea8886
Kotchakorn Wongwisutigul <icekotcha@gmail.com>
2023-10-01 18:36:12 +0700

    refactor: :fire: Remove frontend docker build skip & Fix multiple-machines connection problems

--- frontend/src/pages/api/oauth/login/index.ts
+++ frontend/src/pages/api/oauth/login/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { isQuerySet, getRequestUrl } from '@/utils/sdk'
 import logger from '@/utils/logger'
 import oryHydra from '@/utils/sdk/ory-hydra'
-import { ORY_KRATOS_BROWSER_URL, ORY_KRATOS_INTERNAL_API_URL, oryKratosInternalApi } from '@/utils/sdk/ory-kratos'
+import { ORY_KRATOS_BROWSER_URL, oryKratosInternalApi } from '@/utils/sdk/ory-kratos'
 import { ResponseError } from '@/types/error'
 
 const redirectToLogin = (req: NextApiRequest, res: NextApiResponse) => {
@@ -12,7 +12,7 @@ const redirectToLogin = (req: NextApiRequest, res: NextApiResponse) => {
 
   const baseUrl = getRequestUrl(req)
 
-  logger.debug('Return to: ', {
+  logger.debug('to: ', {
     url: req.url,
     base: baseUrl,
     'kratos.browser': ORY_KRATOS_BROWSER_URL,
@@ -21,9 +21,9 @@ const redirectToLogin = (req: NextApiRequest, res: NextApiResponse) => {
   const returnTo = new URL(req.url as string, baseUrl)
   logger.debug(`returnTo: "${returnTo.toString()}"`, returnTo)
 
-  logger.debug('new URL: ', [ORY_KRATOS_INTERNAL_API_URL + '/self-service/login/browser', baseUrl])
+  logger.debug('new URL: ', [ORY_KRATOS_BROWSER_URL + '/self-service/login/browser', baseUrl])
 
-  const redirectTo = new URL(ORY_KRATOS_INTERNAL_API_URL + '/self-service/login/browser', baseUrl)
+  const redirectTo = new URL(ORY_KRATOS_BROWSER_URL + '/self-service/login/browser', baseUrl)
   redirectTo.searchParams.set('refresh', 'true')
   redirectTo.searchParams.set('return_to', returnTo.toString())
 
@@ -35,6 +35,7 @@ const redirectToLogin = (req: NextApiRequest, res: NextApiResponse) => {
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const loginChallenge = req.query.login_challenge?.toString()
+    console.log('🚀 ~ file: index.ts:38 ~ handler ~ loginChallenge:', loginChallenge)
     if (!isQuerySet(loginChallenge)) {
       const error = new ResponseError('login_challenge is not set', 400)
       throw error
@@ -42,19 +43,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { data: loginRequest } = await oryHydra.getOAuth2LoginRequest({
       loginChallenge,
     })
+    console.log('🚀 ~ file: index.ts:46 ~ handler ~ loginRequest:', loginRequest)
     if (loginRequest.skip) {
       logger.debug('Accepting ORY Hydra Login Request because skip is true')
-      return oryHydra
-        .acceptOAuth2LoginRequest({
-          loginChallenge,
-          acceptOAuth2LoginRequest: {
-            subject: loginRequest.subject,
-          },
-        })
-        .then(({ data }) => res.redirect(String(data.redirect_to)))
+      const { data } = await oryHydra.acceptOAuth2LoginRequest({
+        loginChallenge,
+        acceptOAuth2LoginRequest: {
+          subject: loginRequest.subject,
+        },
+      })
+      res.redirect(String(data.redirect_to))
     }
 
     const kratosSessionCookie = req.cookies.ory_kratos_session
+    console.log('🚀 ~ file: index.ts:60 ~ handler ~ kratosSessionCookie:', kratosSessionCookie)
     if (!kratosSessionCookie) {
       // The state was set but we did not receive a session. Let's retry.
       logger.debug('Redirecting to login page because no ORY Kratos session cookie was set.')
@@ -62,6 +64,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return
     }
 
+    console.log('🚀 ~ file: index.ts:70 ~ handler ~ req.headers.cookie:', req.headers.cookie)
     //  Get the session
     const { data: session } = await oryKratosInternalApi.toSession({
       cookie: req.headers.cookie,
@@ -77,11 +80,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         context: session,
       },
     })
-    return res.redirect(String(response.redirect_to))
+    console.log('🚀 ~ file: index.ts:84 ~ handler ~ response:', response)
+    res.redirect(String(response.redirect_to))
+    console.log('🚀 ~ file: index.ts:85 ~ handler ~ esponse.redirect_to:', response.redirect_to)
   } catch (error) {
+    console.log('🚀 ~ file: index.ts:82 ~ handler ~ error:', error.message)
     if (error instanceof ResponseError) {
-      return res.status(error.status || 500).json({ message: error.message })
+      res.status(error.status || 500).json({ message: error.message })
     }
-    return res.status(500).json({ message: error })
+    res.status(500).json({ message: error })
   }
 }
